ba74001db5f9b931623f16ebd88018c9d3d9f8b4
deepam20050 <88540910+deepam20050@users.noreply.github.com>
2022-07-08 11:00:55 +0530

    Delete parishram directory

--- parishram/1524B brute.py
+++
@@ -1,8 +0,0 @@
-a, b = 1, 1
-q = {1}
-for i in range(0, 10**2) :
-  x = q
-  q.pop(0)
-  print(x)
-  q.append(x * a)
-  q.append(x + b) 
--- parishram/1646 test.py
+++
@@ -1,6 +0,0 @@
-a = 0
-f = 1
-for i in range(1, 16):
-  f = f * i
-  a = a + f
-print(f)
